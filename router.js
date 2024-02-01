08beca1d101499aec8ad62e17b92662fff25c915
ohseungmin <ohmin2886@gmail.com>
2024-02-01 12:08:54 +0900

    sse 설정과 읽을 파일을 지정

--- router.js
+++ router.js
@@ -8,4 +8,33 @@ router.get('/', (req, res) => {
   res.sendFile(readData);
 })
 
+router.get('/sse', (req, res) => {
+  res.writeHead(200, {
+    'Content-Type' : 'text/event-stream',
+    'Cache-Control' : 'no-cache',
+    'Connection' : 'keep-alive',
+  });
+
+  function sendSSEMessage(event, data) {
+    res.write(`event: ${event}\n`);
+    res.write(`data: ${JSON.stringify(data)}\n\n`);
+  }
+
+  fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, jsonData) => {
+    if (err) {
+      console.error("Error reading data.json:", err);
+      res.end();
+      return;
+    }
+
+    const data = JSON.parse(jsonData)
+
+    sendSSEMessage('data', data);
+
+    req.on('close', () => {
+      res.end();
+    });
+  });
+});
+
 module.exports = router
